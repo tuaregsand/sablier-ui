179a35f631bc453b2af28b32e8dc0172d2d9a33f
Your Name <your-email@example.com>
2025-03-04 16:19:07 +0100

    Fix rollup config and build package

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,10 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { dts } from "rollup-plugin-dts";
-import pkg from './package.json' assert { type: 'json' };
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
 
 export default [
   {
